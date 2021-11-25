d0bd45d32a14c12574cf7fd666c316d47c36137a
lesjok <lesjok@inbox.ru>
2021-11-25 18:04:47 +0300

    alert

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const block = document.querySelector('.block-1');
 const blockLeft = document.getElementById('block-left');
 const blockCenter = document.getElementById('block-center');
 
-// alert('Привет мир!');
+alert('Привет мир!');
 
 btnFirst.addEventListener('click', () => {
   block.classList.toggle('hidden');
