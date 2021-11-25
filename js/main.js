233d4a281a9f50c0c521eba266ca794c700d1eea
lesjok <lesjok@inbox.ru>
2021-11-25 18:11:06 +0300

    jquery

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
+$(function() {
+  const btnFirst = document.querySelector('.btn-first');
+  const btnSecond = document.querySelector('.btn-second');
+  const block = document.querySelector('.block-1');
+  const blockLeft = document.getElementById('block-left');
+  const blockCenter = document.getElementById('block-center');
 
-const btnFirst = document.querySelector('.btn-first');
-const btnSecond = document.querySelector('.btn-second');
-const block = document.querySelector('.block-1');
-const blockLeft = document.getElementById('block-left');
-const blockCenter = document.getElementById('block-center');
+  alert('Привет мир!');
 
-alert('Привет мир!');
+  btnFirst.addEventListener('click', () => {
+    block.classList.toggle('hidden');
+  })
 
-btnFirst.addEventListener('click', () => {
-  block.classList.toggle('hidden');
+  btnSecond.addEventListener('click', () => {
+    blockLeft.classList.toggle('translate-left');
+    blockCenter.classList.toggle('translate-center');
+  })
 })
 
-btnSecond.addEventListener('click', () => {
-  blockLeft.classList.toggle('translate-left');
-  blockCenter.classList.toggle('translate-center');
-})
