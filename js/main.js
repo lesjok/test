335eceda55f93beb59f5e1e19a18c56363030b45
lesjok <lesjok@inbox.ru>
2021-11-25 18:01:05 +0300

    css, js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+const btnFirst = document.querySelector('.btn-first');
+const btnSecond = document.querySelector('.btn-second');
+const block = document.querySelector('.block-1');
+const blockLeft = document.getElementById('block-left');
+const blockCenter = document.getElementById('block-center');
+
+// alert('Привет мир!');
+
+btnFirst.addEventListener('click', () => {
+  block.classList.toggle('hidden');
+})
+
+btnSecond.addEventListener('click', () => {
+  blockLeft.classList.toggle('translate-left');
+  blockCenter.classList.toggle('translate-center');
+})
